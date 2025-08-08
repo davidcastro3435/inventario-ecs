4e3ca912bdb3fb57330334d1d7e764b05ca3f42b
davidcastro3435 <josue@josucastro.dev>
2025-08-08 17:51:57 -0600

    feat: Creación y estilización del login

---
+++ frontend/src/js/auth.js
@@ -0,0 +1,7 @@
+// auth.js - Lógica de autenticación para Login
+
+// Aquí irá la lógica para manejar el envío del formulario y la autenticación de usuario
+// document.querySelector('.auth-form').addEventListener('submit', function(e) {
+//     e.preventDefault();
+//     // Lógica de autenticación aquí
+// });
