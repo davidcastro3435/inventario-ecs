497520d4383d5f1e0a06b53928f063d4833e8f07
davidcastro3435 <josue@josucastro.dev>
2025-08-12 17:42:57 -0600

    feat: Implementar CORS en el servidor

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import inventarioRoutes from './src/routes/inventario.js';
 import bodyParser from 'body-parser';
+import cors from 'cors';
 
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.json()); // Middleware to parse JSON bodies
+// Habilitar CORS para todas las rutas
+app.use(cors());
 
 app.use('/inventario', inventarioRoutes);
 
