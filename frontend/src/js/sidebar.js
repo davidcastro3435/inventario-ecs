d8510fc00e69535b0e25bfaebc204e4f4bec3042
davidcastro3435 <josue@josucastro.dev>
2025-09-20 13:42:17 -0600

    feat: Validar el token para redigir según el rol

--- frontend/src/js/sidebar.js
+++ frontend/src/js/sidebar.js
@@ -12,7 +12,19 @@ export function setupSidebarNavigation() {
 	}
 	if (inventarioBtn) {
 		inventarioBtn.addEventListener('click', function() {
-			window.location.href = '../pages/inventory.html';
+            const token = localStorage.getItem('token');
+            const tokenDecodificado = decodificarJWT(token);
+
+			if (tokenDecodificado) {
+                // Comprobar el rol
+                if (tokenDecodificado.rol === 'admin') {
+                    window.location.href = '../pages/inventory.html';
+                } else {
+                    window.location.href = '../pages/inventarioBasico.html';
+                }
+			} else {
+				console.error('Token no encontrado en localStorage');
+			}
 		});
 	}
 	if (bitacoraBtn) {
@@ -28,4 +40,29 @@ export function setupSidebarNavigation() {
 			alert('La página de configuraciones aún no está disponible.');
 		});
 	}
+}
+
+function decodificarJWT(token) {
+    try {
+        if (!token || token.split('.').length !== 3) {
+            throw new Error("Token JWT inválido");
+        }
+
+        // Separar el token en sus partes (header, payload, signature)
+        const base64Url = token.split('.')[1];
+        
+        // Convertir de Base64Url a Base64
+        const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+        
+        // Decodificar la parte Base64
+        const decodedPayload = atob(base64);
+        
+        // Convertir el payload de texto JSON a un objeto
+        const jsonPayload = JSON.parse(decodedPayload);
+        
+        return jsonPayload;  // Devuelve el objeto con los datos del payload
+    } catch (error) {
+        console.error('Error al decodificar el JWT:', error);
+        return null;  // En caso de error, retorna null
+    }
 }
