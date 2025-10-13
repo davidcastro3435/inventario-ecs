aaf73cafe84abc766ee6d82bc097043124658ac5
davidcastro3435 <josue@josucastro.dev>
2025-10-13 15:30:27 -0600

    feat: Agregar servicio para actualizar contraseña y correo de usuario

--- frontend/src/services/usuarioService.js
+++ frontend/src/services/usuarioService.js
@@ -1,7 +1,40 @@
+// usuarioService.js
+// Lógica para conectar con el API de usuario
+
 function getAuthHeaders() {
   const token = localStorage.getItem('token');
   if (token) {
     return { 'Authorization': 'Bearer ' + token };
   }
   return {};
+}
+
+// Funcion para actualizar contraseña del usuario
+export async function patchContrasenaUsuario(id, data) {
+  const userId = localStorage.getItem('userId');
+  const response = await fetch(`http://localhost:3000/usuario/datos/contrasena`, {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json',
+      ...getAuthHeaders()
+    },
+    body: JSON.stringify({ ...data, id_usuario: userId }),
+  });
+  if (!response.ok) throw new Error('Error al modificar la contraseña del usuario');
+  return await response.json();
+}
+
+// Funcion para actualizar correo del usuario
+export async function patchCorreoUsuario(id, data) {
+  const userId = localStorage.getItem('userId');
+  const response = await fetch(`http://localhost:3000/usuario/datos/correo`, {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json',
+      ...getAuthHeaders()
+    },
+    body: JSON.stringify({ ...data, id_usuario: userId }),
+  });
+  if (!response.ok) throw new Error('Error al modificar el correo del usuario');
+  return await response.json();
 }
